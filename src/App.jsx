9b6c5806ad1f8bd5c5195adb6242530106d8d72d
Emre Ceyhan <emre.ceyhan@wanderlens.net>
2025-12-22 02:05:56 +0300

    test

--- src/App.jsx
+++ src/App.jsx
@@ -1,54 +1,67 @@
 import { Route, Routes } from "react-router-dom";
-import "./App.css";
+import { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { refreshThunk } from "./redux/auth/operations";
+import { selectRefresh } from "./redux/auth/selectors";
+
 import HomePage from "./pages/HomePage";
 import LoginPage from "./pages/LoginPage";
 import RegisterPage from "./pages/Register";
 import TransactionsPage from "./pages/Transactions";
 import StatisticPage from "./pages/Statistics";
 import CurrencyPage from "./pages/CurrencyPage";
-import { useMediaQuery, useTheme } from "@mui/material";
 import NotFound from "./pages/NotFound";
 import PrivateRoute from "./components/Route/PrivateRoute";
 
 function App() {
-  const theme = useTheme();
-  const isMobile = useMediaQuery(theme.breakpoints.down("sm"));
+  const dispatch = useDispatch();
+  const isRefreshing = useSelector(selectRefresh);
+
+  // 🔥 refresh SADECE BURADA
+  useEffect(() => {
+    dispatch(refreshThunk());
+  }, [dispatch]);
+
+  // ⛔ auth hazır değilken route render ETME
+  if (isRefreshing) {
+    return null; // veya Loader
+  }
 
   return (
-    <>
-      <Routes>
-        <Route path="/" element={<HomePage />} />
-        <Route path="/login" element={<LoginPage />} />
-        <Route path="/register" element={<RegisterPage />} />
-        <Route
-          path="/transactions"
-          element={
-            <PrivateRoute>
-              <TransactionsPage />
-            </PrivateRoute>
-          }
-        />
-        <Route
-          path="/statistics"
-          element={
-            <PrivateRoute>
-              <StatisticPage />
-            </PrivateRoute>
-          }
-        />
-        {isMobile && (
-          <Route
-            path="/currency"
-            element={
-              <PrivateRoute>
-                <CurrencyPage />
-              </PrivateRoute>
-            }
-          />
-        )}
-        <Route path="/*" element={<NotFound />} />
-      </Routes>
-    </>
+    <Routes>
+      <Route path="/" element={<HomePage />} />
+      <Route path="/login" element={<LoginPage />} />
+      <Route path="/register" element={<RegisterPage />} />
+
+      <Route
+        path="/transactions"
+        element={
+          <PrivateRoute>
+            <TransactionsPage />
+          </PrivateRoute>
+        }
+      />
+
+      <Route
+        path="/statistics"
+        element={
+          <PrivateRoute>
+            <StatisticPage />
+          </PrivateRoute>
+        }
+      />
+
+      <Route
+        path="/currency"
+        element={
+          <PrivateRoute>
+            <CurrencyPage />
+          </PrivateRoute>
+        }
+      />
+
+      <Route path="*" element={<NotFound />} />
+    </Routes>
   );
 }
 
--- src/components/Route/PrivateRoute.jsx
+++ src/components/Route/PrivateRoute.jsx
@@ -1,21 +1,9 @@
 import { Navigate } from "react-router-dom";
-import { useDispatch, useSelector } from "react-redux";
+import { useSelector } from "react-redux";
 import { selectLoggedIn } from "../../redux/auth/selectors";
-import { useEffect } from "react";
-import { refreshThunk } from "../../redux/auth/operations";
 
 export default function PrivateRoute({ children }) {
-  const dispatch = useDispatch();
-
-  useEffect(() => {
-    dispatch(refreshThunk());
-  }, [dispatch]);
-
   const isLoggedIn = useSelector(selectLoggedIn);
 
-  if (!isLoggedIn) {
-    return <Navigate to="/login" replace />;
-  }
-
-  return children;
+  return isLoggedIn ? children : <Navigate to="/login" replace />;
 }
