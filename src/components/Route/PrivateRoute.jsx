92858029a254f6da75fe4cb633e45f47997136d0
Emre Ceyhan <emre.ceyhan@wanderlens.net>
2025-12-22 02:21:12 +0300

    test

--- src/components/Route/PrivateRoute.jsx
+++ src/components/Route/PrivateRoute.jsx
@@ -1,9 +1,21 @@
 import { Navigate } from "react-router-dom";
-import { useSelector } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import { selectLoggedIn } from "../../redux/auth/selectors";
+import { useEffect } from "react";
+import { refreshThunk } from "../../redux/auth/operations";
 
 export default function PrivateRoute({ children }) {
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(refreshThunk());
+  }, [dispatch]);
+
   const isLoggedIn = useSelector(selectLoggedIn);
 
-  return isLoggedIn ? children : <Navigate to="/login" replace />;
+  if (!isLoggedIn) {
+    return <Navigate to="/login" replace />;
+  }
+
+  return children;
 }
